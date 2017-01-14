6220ff9de699744c0ed4f48d4904731161d5dd6f
novokrest <novokrest013@gmail.com>
2017-01-14 22:08:21 +0300

    show exception message on error page

--- server/src/main/java/com/oneuse/dainbow/web/WebAppWideExceptionHandler.java
+++ server/src/main/java/com/oneuse/dainbow/web/WebAppWideExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.oneuse.dainbow.web;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class WebAppWideExceptionHandler {
-    @ExceptionHandler(RuntimeException.class)
-    public String runtimeExceptionHandler() {
+    @ExceptionHandler(Exception.class)
+    public String runtimeExceptionHandler(Exception ex, Model model) {
+        model.addAttribute("exception", ex);
         return "error";
     }
 }
--- server/src/main/webapp/WEB-INF/views/error.jsp
+++ server/src/main/webapp/WEB-INF/views/error.jsp
@@ -1,1 +1,8 @@
-<h4>Something went wrong...</h4
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page isELIgnored="false" %>
+<%@ page session="false" %>
+
+<h4>Something went wrong...</h4>
+<p>
+    <c:out value="${exception.message}" />
+</p>
