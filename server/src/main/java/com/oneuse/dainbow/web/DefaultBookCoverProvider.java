33b3490a6d94a6a01b1a951306843c89a74bc286
novokrest <novokrest013@gmail.com>
2017-01-14 22:15:34 +0300

    update DefaultBookCoverProvider: try find cover image in DB and failed case return default cover

--- server/src/main/java/com/oneuse/dainbow/web/DefaultBookCoverProvider.java
+++ server/src/main/java/com/oneuse/dainbow/web/DefaultBookCoverProvider.java
@@ -3,6 +3,7 @@ package com.oneuse.dainbow.web;
 import com.oneuse.dainbow.BookCoverProvider;
 import com.oneuse.dainbow.image.Image;
 import com.oneuse.dainbow.image.ImageType;
+import com.oneuse.dainbow.storage.JdbcBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,15 +15,25 @@ import java.io.File;
 
 @Component
 public class DefaultBookCoverProvider implements BookCoverProvider {
+    private final JdbcBookRepository bookRepository;
     private final ServletContext context;
 
     @Autowired
-    public DefaultBookCoverProvider(ServletContext context) {
+    public DefaultBookCoverProvider(JdbcBookRepository bookRepository, ServletContext context) {
+        this.bookRepository = bookRepository;
         this.context = context;
     }
 
     @Override
     public Image findCover(long bookId) {
+        Image cover = bookRepository.findOne(bookId).getCoverImage();
+        if (cover == null) {
+            return loadDefaultCover();
+        }
+        return cover;
+    }
+
+    private Image loadDefaultCover() {
         try {
             ByteArrayOutputStream imgOutputStream = new ByteArrayOutputStream();
             File bookCoverImgFile = new File(context.getRealPath("resources/img/book-cover.png"));
