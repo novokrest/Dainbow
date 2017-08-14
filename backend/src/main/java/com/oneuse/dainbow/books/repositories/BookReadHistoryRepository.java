51735823162062e7bdd39534c6ac62377b499b31
novokrest <novokrest013@gmail.com>
2017-08-14 17:42:50 +0300

    [backend] BookReadHistoryRepository: change path from activity to history

--- backend/src/main/java/com/oneuse/dainbow/books/repositories/BookReadHistoryRepository.java
+++ backend/src/main/java/com/oneuse/dainbow/books/repositories/BookReadHistoryRepository.java
@@ -4,6 +4,6 @@ import com.oneuse.dainbow.books.repositories.entity.BookReadActivityEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource(path = "activity", rel = "activity")
+@RestResource(path = "history", rel = "history")
 public interface BookReadHistoryRepository extends CrudRepository<BookReadActivityEntity, Long> {
 }
