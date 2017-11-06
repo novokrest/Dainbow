624c8fdcee29b539993522122546c208c1a83492
novokrest <novokrest013@gmail.com>
2017-11-06 19:00:11 +0300

    gcloud: in progress

--- backend-new/src/main/resources/sql/v6__insert_book_read_progress_data.sql
+++ backend-new/src/main/resources/sql/v6__insert_book_read_progress_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO book_read_progress (id, book_id, read_pages_count) VALUES (1, 1, 200);
-INSERT INTO book_read_progress (id, book_id, read_pages_count) VALUES (2, 2, 500);
-INSERT INTO book_read_progress (id, book_id, read_pages_count) VALUES (3, 3, 400);
-INSERT INTO book_read_progress (id, book_id, read_pages_count) VALUES (4, 4, 700);
-INSERT INTO book_read_progress (id, book_id, read_pages_count) VALUES (5, 5, 100);
+INSERT INTO BOOK_READ_PROGRESS (id, book_id, read_pages_count) VALUES (1, 1, 200);
+INSERT INTO BOOK_READ_PROGRESS (id, book_id, read_pages_count) VALUES (2, 2, 500);
+INSERT INTO BOOK_READ_PROGRESS (id, book_id, read_pages_count) VALUES (3, 3, 400);
+INSERT INTO BOOK_READ_PROGRESS (id, book_id, read_pages_count) VALUES (4, 4, 700);
+INSERT INTO BOOK_READ_PROGRESS (id, book_id, read_pages_count) VALUES (5, 5, 100);
--- backend-new/src/main/resources/sql/v7__insert_book_read_history_data.sql
+++ backend-new/src/main/resources/sql/v7__insert_book_read_history_data.sql
@@ -1,14 +1,14 @@
-INSERT INTO book_read_history (id, book_id, begin_page, end_page, begin_time, end_time)
+INSERT INTO BOOK_READ_HISTORY (id, book_id, begin_page, end_page, begin_time, end_time)
 VALUES (1, 1, 11, 55, '2015-10-15 12:00:00', '2015-10-15 13:00:00');
 
-INSERT INTO book_read_history (id, book_id, begin_page, end_page, begin_time, end_time)
+INSERT INTO BOOK_READ_HISTORY (id, book_id, begin_page, end_page, begin_time, end_time)
 VALUES (2, 2, 33, 55, '2015-11-15 13:00:00', '2015-11-15 15:00:00');
 
-INSERT INTO book_read_history (id, book_id, begin_page, end_page, begin_time, end_time)
+INSERT INTO BOOK_READ_HISTORY (id, book_id, begin_page, end_page, begin_time, end_time)
 VALUES (3, 2, 50, 100, '2015-12-15 13:00:00', '2015-12-15 19:00:00');
 
-INSERT INTO book_read_history (id, book_id, begin_page, end_page, begin_time, end_time)
+INSERT INTO BOOK_READ_HISTORY (id, book_id, begin_page, end_page, begin_time, end_time)
 VALUES (4, 3, 11, 55, '2015-10-15 12:00:00', '2015-10-15 13:00:00');
 
-INSERT INTO book_read_history (id, book_id, begin_page, end_page, begin_time, end_time)
+INSERT INTO BOOK_READ_HISTORY (id, book_id, begin_page, end_page, begin_time, end_time)
 VALUES (5, 4, 11, 55, '2015-10-15 12:00:00', '2015-10-15 13:00:00');
