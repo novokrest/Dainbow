2aec1b5a7726285eae702dc443ac4bb980a9234e
novokrest <novokrest013@gmail.com>
2017-11-26 10:19:15 +0300

    in progress: rebase this

---
+++ backend-new/aws-mysql.sh
@@ -0,0 +1,1 @@
+mysql -h aws-mysql-react.coyijqepd2cf.us-east-2.rds.amazonaws.com -P 3306 -u aws_user -p
---
+++ backend-new/docker-mysql.sh
@@ -0,0 +1,1 @@
+docker run --rm --name docker-mysql -v /Volumes/DATA/docker/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=qwe -p 4407:3306 mysql:latest
