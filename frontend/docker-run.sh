99218b0f84e3b558610eb1fcb40545a5798848d4
novokrest <novokrest013@gmail.com>
2017-11-28 01:58:51 +0300

    [front] update docker-run.sh

--- frontend/docker-run.sh
+++ frontend/docker-run.sh
@@ -2,5 +2,11 @@
 
 IMAGE=dnovokrest/dainbow-react-frontend
 
+BACKGROUND_FLAG=-d
+
+if [ $# -gt 0 ] && [ $1 == "debug" ]; then
+  BACKGROUND_FLAG=
+fi
+
 docker pull $IMAGE
-docker run -it --rm --name react-frontend -p 80:8080 -d $IMAGE
+docker run -it --rm --name react-frontend -p 80:8080 $BACKGROUND_FLAG $IMAGE
