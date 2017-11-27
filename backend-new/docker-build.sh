4554086082127a24f949435e1ead840f34717fd2
novokrest <novokrest013@gmail.com>
2017-11-28 01:41:56 +0300

    [backend] add docker related scripts

--- backend-new/docker-build.sh
+++ backend-new/docker-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-IMAGE=dainbow-react-backend
-USER=dnovokrest
-TAG=latest
-REPO_IMAGE=$USER/$IMAGE:$TAG
+IMAGE=dnovokrest/dainbow-react-backend:latest
 
+./gradlew build
 docker build --build-arg JAR_FILE=build/libs/dainbow-server-backend-1.0.0.jar -t $IMAGE .
-docker tag $IMAGE $REPO_IMAGE 
-docker push $REPO_IMAGE
+
+if [ $# -gt 0 ]; then
+  echo Pushing docker image: $IMAGE
+  docker push $IMAGE
+fi
--- backend-new/docker-run.sh
+++ backend-new/docker-run.sh
@@ -1,1 +1,18 @@
-docker run -e SPRING_PROFILES_ACTIVE=aws -p 4003:8080 dainbow-react-backend
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo Few arguments: specify Spring profile e.g. dev
+  exit 1
+fi
+
+BACKGROUND_FLAG=-d
+
+if [ $# -gt 1 ] && [ $2 == "debug" ]; then
+  BACKGROUND_FLAG=
+fi  
+
+docker run -it --rm \
+  --name react-backend \
+  -e SPRING_PROFILES_ACTIVE=$1 \
+  -p 8080:8080 \
+  $BACKGROUND_FLAG dnovokrest/dainbow-react-backend
