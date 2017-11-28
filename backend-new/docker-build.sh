f3a6e2838cd25a5ca9ce67971ba6acbdfe917921
novokrest <novokrest013@gmail.com>
2017-11-29 01:34:07 +0300

    update docker-build.sh

--- backend-new/docker-build.sh
+++ backend-new/docker-build.sh
@@ -5,7 +5,7 @@ IMAGE=dnovokrest/dainbow-react-backend:latest
 ./gradlew build
 docker build --build-arg JAR_FILE=build/libs/dainbow-server-backend-1.0.0.jar -t $IMAGE .
 
-if [ $# -gt 0 ]; then
+if [ $# -gt 0 ] && [ $1 == "push" ]; then
   echo Pushing docker image: $IMAGE
   docker push $IMAGE
 fi
