6a9f4e8473b03fc50496091ee1819256f6a68616
novokrest <novokrest013@gmail.com>
2018-02-03 14:25:06 +0300

    in progress

---
+++ accounter/src/main/java/com/oneuse/dainbow/accounter/dainbowaccounter/DainbowAccounterApplication.java
@@ -0,0 +1,12 @@
+package com.oneuse.dainbow.accounter.dainbowaccounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainbowAccounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DainbowAccounterApplication.class, args);
+	}
+}
