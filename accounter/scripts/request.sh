391cb269680e94691299de8fd61e26774daf57b6
novokrest <novokrest013@gmail.com>
2018-02-08 04:50:11 +0300

    in progress

---
+++ accounter/scripts/request.sh
@@ -0,0 +1,1 @@
+curl -kv 'https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http://localhost:30005/dainbow/accounter/oauth2&response_type=code&client_id=1069112770187-86kp601ddg7bt7m4kvf1s6pokmienrob.apps.googleusercontent.com'
